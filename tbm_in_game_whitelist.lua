6252ea17555c4e4d61be79eebdc07a4fb53d0858
Ralf Geisthardt <ralfge@gmail.com>
2022-05-13 17:26:18 +0200

    Update tbm_in_game_whitelist.lua

--- tbm_in_game_whitelist.lua
+++ tbm_in_game_whitelist.lua
@@ -1,5 +1,6 @@
 Config = {}
-Config.Locale = 'en'
+Config.Locale = 'de'
+Config.useQBCore = false
 Config.EventsThatShouldTriggerAfterSuccessfulWhitelist = { -- you can add here as many events you want. The event receiver get always as first parameter the source of the white listed player
     {
         sendTo = 'client', -- Value can be client or server
@@ -12,13 +13,30 @@ Config.EventsThatShouldTriggerAfterSuccessfulWhitelist = { -- you can add here a
         active = false, -- enable or disable sending values are true or false
     },
 }
-Config.waitTimeInSecondsIfTestWasNotCorrect = 3600
-Config.maxWhiteListTries = 3
-Config.importExistingPlayersIntoWhitelistAtFirstStart = false
-Config.howMuchQuestionsWillAnswered = 2
-Config.howMuchQuestionsMustAnsweredCorrect = 2
-Config.adminIds = {
-    'steam:11000010e11de1d'
+Config.waitTimeInSecondsIfTestWasNotCorrect = 3600  -- how long the player must wait for a retry
+Config.maxWhiteListTries = 3 -- max tries for a player
+Config.importExistingPlayersIntoWhitelistAtFirstStart = false -- all known players will automatically imported
+Config.howMuchQuestionsWillAnswered = 2 -- How many question will ask from Questions below
+Config.howMuchStaticQuestionsMustAnsweredCorrect = 2 -- How much questions from StaticQuestionsAtTheEnd will ask
+Config.howMuchQuestionsMustAnsweredCorrect = 4 -- How much questions must be answered correct from StaticQuestionsAtTheEnd and from Questions
+Config.adminIds = { -- this players can use the admin commands
+    'steam:xxxxx',
+}
+
+Config.useOnlyLicenceId = true -- use licence id instead of steam id for whitlisted players
+
+Config.discord = {
+    useDiscordRoleCheck = true, -- if players rejoin your server after whitelist the discord role will assigned
+    useDiscordRoleAssignment = true, -- if true then the roles whitelistRoles below will assigned
+    useDiscordRoleRemove = true, -- if true then the roles in rolesToRemoveWhenWhitelisted below will removed
+    guildId = 0, -- add your guild id here
+    botToken = "enter your token here", -- copy that token from boot creation
+    whitelistRoles = { -- after white list pass this, roles will assigned
+        "0000000000000000" -- add your role id here
+    },
+    rolesToRemoveWhenWhitelisted = { -- after white list pass this, roles will removed
+        "0000000000000000"
+    }
 }
 
 Questions = {
@@ -38,5 +56,24 @@ Questions = {
             "lorem ipsum answer 2"
         },
         correctAnswer = 1
-    }
+    },
+}
+
+StaticQuestionsAtTheEnd = {
+    {
+        question = "I agree that i am 18 Years old.",
+        answers = {
+            "I confirm",
+            "No, iam not.",
+        },
+        correctAnswer = 1
+    },
+    {
+        question = "Lorem ipsum",
+        answers = {
+            "lorem ipsum <b>answer</b> 1 (this is correct)",
+            "lorem ipsum answer 2"
+        },
+        correctAnswer = 1
+    },
 }

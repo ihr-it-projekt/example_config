4f3f6f8db8667ef3947898f001dd727e71b02921
Ralf Geisthardt <ralfge@gmail.com>
2022-06-25 20:23:57 +0200

    1.12.0

--- tbm_in_game_whitelist.lua
+++ tbm_in_game_whitelist.lua
@@ -1,5 +1,5 @@
 Config = {}
-Config.Locale = 'de'
+Config.Locale = 'en'
 Config.useQBCore = false
 Config.EventsThatShouldTriggerAfterSuccessfulWhitelist = { -- you can add here as many events you want. The event receiver get always as first parameter the source of the white listed player
     {
@@ -17,25 +17,28 @@ Config.waitTimeInSecondsIfTestWasNotCorrect = 3600  -- how long the player must 
 Config.maxWhiteListTries = 3 -- max tries for a player
 Config.importExistingPlayersIntoWhitelistAtFirstStart = false -- all known players will automatically imported
 Config.howMuchQuestionsWillAnswered = 2 -- How many question will ask from Questions below
-Config.howMuchStaticQuestionsMustAnsweredCorrect = 2 -- How much questions from StaticQuestionsAtTheEnd will ask
-Config.howMuchQuestionsMustAnsweredCorrect = 4 -- How much questions must be answered correct from StaticQuestionsAtTheEnd and from Questions
+Config.howMuchQuestionsMustAnsweredCorrect = 2 -- How many questions must be answered correct from Questions below
+Config.howMuchStaticQuestionsMustAnsweredCorrect = 2 -- How many questions from StaticQuestionsAtTheEnd must answered correct
 Config.adminIds = { -- this players can use the admin commands
     'steam:xxxxx',
 }
-
-Config.useOnlyLicenceId = true -- use licence id instead of steam id for whitlisted players
 
 Config.discord = {
     useDiscordRoleCheck = true, -- if players rejoin your server after whitelist the discord role will assigned
     useDiscordRoleAssignment = true, -- if true then the roles whitelistRoles below will assigned
     useDiscordRoleRemove = true, -- if true then the roles in rolesToRemoveWhenWhitelisted below will removed
-    guildId = 0, -- add your guild id here
-    botToken = "enter your token here", -- copy that token from boot creation
+    guildId = 000000000, -- add your guild id here
+    botToken = "some token", -- copy that token from boot creation
+    enablePreCheckForDiscordRule = true, -- pre check rule enable
+    preCheckRules = { -- must have one of that roles before player can start whitelist
+        "0",
+        "1",
+    },
     whitelistRoles = { -- after white list pass this, roles will assigned
-        "0000000000000000" -- add your role id here
+        "0" -- add your role id here
     },
     rolesToRemoveWhenWhitelisted = { -- after white list pass this, roles will removed
-        "0000000000000000"
+        "0"
     }
 }
 

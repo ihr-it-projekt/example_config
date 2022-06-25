356650f767d59d467ec845d0f210606053ec325a
Ralf Geisthardt <ralfge@gmail.com>
2022-06-25 08:51:41 +0200

    Version 1.3.0

--- TBM_prevent_double_connections.lua
+++ TBM_prevent_double_connections.lua
@@ -1,15 +1,22 @@
 Config = {
-    checkInterval = 60, -- Value is in seconds. How often the player double connection will checked. You can increase that value. But if this value is to high yor player can get problems when he reconnect
     messages = { -- messages that the player got
         double_connection_detected = 'Doppelte Verbindung erkannt. Du wurdest reportet.', -- if a player has tow connections to server
         no_player_ids_found = 'Deine Discord oder SteamId oder Rockstar ID konnte nicht gelesene werden.' -- if no steam or licence or discord id can be found, player got this message
     },
-
     discord = { -- Discord configuration
         enableLog = true, -- enable log to discord
         webhookUrl = '', -- create a webhock url an paste the link here in
         color = '5015295', -- color of message
         messageTitle = 'Double connection discovered', -- Discord message title
         message = 'Player Data: ', -- prefix in discord message. Player data will automatically appended
-    }
+    },
+    banOptions = {
+        banPlayerInTBMInGameWhiteList = false, -- If you have my mod: TBM In Game Whitelist (https://thebusters-shop.tebex.io/package/5070248). The player can be baned there.
+    },
+    EventsThatShouldTriggerDoubleConnectionDetected = { -- you can add here as many events you want. They will trigger a server side event with parameters: TriggerEvent(yourEventName, playerIdentityData), Take a look into readme for closer informations
+        {
+            name = 'yourEventName', -- The event name
+            active = true, -- enable or disable sending, values are true or false
+        },
+    },
 }

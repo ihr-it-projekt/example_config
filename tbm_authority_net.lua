d3ba4b3448ddcc984dce39b5c195478ce755941b
Ralf Geisthardt <ralfge@gmail.com>
2022-08-13 23:55:51 +0200

    Update tbm_authority_net.lua

--- tbm_authority_net.lua
+++ tbm_authority_net.lua
@@ -1,5 +1,5 @@
 Config = {
-    language = 'de',
+    language = 'en',
     useQbCore = false, -- if you are using ESX, change this value to false. If you are using QBCore, set value to true
     qbCoreExportName = 'qb-core', -- for security reasons you can customise the qb-core export name. Attention if you change it, be sure you have also change it in qbCore
     esxEventNames = { -- used esx events can here renamed, if you use a anti cheat tool
@@ -9,142 +9,133 @@ Config = {
         sendMessage = 'esx:showNotification',
         addGroupCommand = 'es:addGroupCommand',
     },
-    jobNamesCops = 'police',
-    viewRangeForMarkers = 100,
-    animation = true,
-    playerServerSlots = 128,
-    unemployedJob = {
+    viewRangeForMarkers = 100, -- how far ranges of markers are visible
+    animation = true, -- Players get a Tablet inclusive animation when he open Authority Net
+    playerServerSlots = 128, -- How many Players can play on your server
+    unemployedJob = { -- what is your unemployed job
         name = 'unemployed',
         grade = 0,
     },
-    police = {
-        positionsStyleConfig = {
-            blip = {
-                enable = true,
-                display = 4,
-                sprite = 499,
-                scale = 1.0,
-                color = 1,
-                name = 'PC',
-            },
-            marker = {
-                enable = true,
-                radius = 3.0,
-                marker = 27,
-                scale = 1.0,
-                rgba = { 255, 0, 0, 155 },
-                subMarker = {
-                    marker = 21,
-                    posZ = 1.0,
-                },
-            },
-        },
-        jobName = 'police',
+    police = { -- On Duty configuration
+        jobName = 'police', -- what is the name of the police job, default is police
         jobGrades = { -- order is important. Please order from lowest grade to highest grade
             {
                 name = 'recruit',
-                label = 'Sheriff',
-                grade = 0,
-            },
-            {
-                name = 'recruit',
                 label = 'Rekrut',
-                grade = 1,
+                grade = 0,
             },
             {
                 name = 'officer',
                 label = 'Officer 1',
-                grade = 2,
+                grade = 1,
             },
             {
                 name = 'detective',
                 label = 'Officer 2',
-                grade = 3,
+                grade = 2,
             },
             {
                 name = 'detective',
                 label = 'Officer 3',
-                grade = 4,
+                grade = 3,
             },
             {
                 name = 'detective',
                 label = 'Detective',
-                grade = 5,
+                grade = 4,
             },
             {
                 name = 'sergeant',
                 label = 'Sergeant 1',
-                grade = 6,
+                grade = 5,
             },
             {
                 name = 'sergeant',
                 label = 'Sergeant 2',
-                grade = 7,
+                grade = 6,
             },
             {
                 name = 'lieutenant',
                 label = 'Lieutenant',
-                grade = 8,
+                grade = 7,
             },
             {
                 name = 'captain',
                 label = 'Captain',
-                grade = 9,
+                grade = 8,
             },
             {
                 name = 'boss',
                 label = 'Co Chief',
-                grade = 10,
+                grade = 9,
             },
             {
                 name = 'boss',
                 label = 'Chief',
-                grade = 11,
+                grade = 10,
             },
         },
-        canOpenEverywhere = true,
-        inEmergencyVehicle = true,
-        vehicleWhereCanOpen = {
-            --'police2'
-        },
-        radiusToOpen = 2.0,
-        positionsWhereCanOpen = {
-            vector3(-1281.9956054688,-3310.4956054688,12.945037841797)
-        },
-        rangeToPositionWhereCanOpen = 2.0,
-        keyToOpen = 57,
-        toggleJob = 'offpolice',
-        isOnDuty = true,
-    },
-    offpolice = {
-        toggleJob = 'police',
-        isOnDuty = false,
-        canOpenEverywhere = true,
-        radiusToOpen = 2.0,
-        positionsWhereCanOpen = {
-            vector3(-1281.9956054688,-3310.4956054688,12.945037841797)
-        },
-        positionsStyleConfig = {
+        canOpenEverywhere = true, -- can open Authority Net from everywhere
+        inEmergencyVehicle = true, -- can open only in emergency vehicles
+        positionsStyleConfig = { -- If you want to open Authority Net on PC, you can configure it here
             blip = {
-                enable = true,
-                display = 4,
-                sprite = 499,
-                scale = 1.0,
-                color = 1,
-                name = 'PC',
+                enable = true, -- will display on map
+                display = 4, -- see displayId on https://docs.fivem.net/natives/?_0x9029B2F3DA924928
+                sprite = 499, -- blip symbol on map, see: https://docs.fivem.net/docs/game-references/blips/
+                scale = 1.0, -- scale of blip
+                color = 1, -- color of blip, see https://docs.fivem.net/docs/game-references/blips/
+                name = 'PC', -- Name of the Blip
             },
             marker = {
-                enable = true,
-                radius = 3.0,
-                marker = 27,
-                scale = 1.0,
-                rgba = { 255, 0, 0, 155 },
+                enable = true, -- will displayed
+                radius = 3.0, -- radius of marker
+                marker = 27, -- marker type see https://docs.fivem.net/docs/game-references/checkpoints/
+                scale = 1.0, -- scale of marker
+                rgba = { 255, 0, 0, 155 }, -- color of marker
                 subMarker = {
-                    marker = 4,
-                    posZ = 1.0,
+                    marker = 21, -- marker type see https://docs.fivem.net/docs/game-references/markers/
+                    posZ = 1.0,  -- z position of the subMarker
                 },
             },
         },
-        keyToOpen = 57,
+        radiusToOpen = 2.0, -- Radius where Authority can be opened on configured positions
+        positionsWhereCanOpen = { -- Position where you have for example your PC and Police can open Authority Net
+            vector3(-1281.9956054688,-3310.4956054688,12.945037841797)
+        },
+        vehicleWhereCanOpen = { -- additional vehicles where Authority Net can be opened
+            --'police2'
+        },
+        keyToOpen = 57, -- Key when you want to open Authority Net from everywhere. See: https://docs.fivem.net/docs/game-references/controls/
+        toggleJob = 'offpolice', -- what is the off duty job name of your police. Only used in ESX
+    },
+    offpolice = {
+        toggleJob = 'police', -- default is "police" change it if your job name is different for police
+        canOpenEverywhere = true, -- off duty cops can open the tablet everywhere
+        radiusToOpen = 2.0, -- Radius where Authority can be opened on configured positions
+        positionsWhereCanOpen = { -- Position where you have for example your PC and Police can open Authority Net
+            vector3(-1281.9956054688,-3310.4956054688,12.945037841797)
+        },
+        positionsStyleConfig = { -- If you want to open Authority Net on PC, you can configure it here
+            blip = {
+                enable = true, -- will display on map
+                display = 4, -- see displayId on https://docs.fivem.net/natives/?_0x9029B2F3DA924928
+                sprite = 499, -- blip symbol on map, see: https://docs.fivem.net/docs/game-references/blips/
+                scale = 1.0, -- scale of blip
+                color = 1, -- color of blip, see https://docs.fivem.net/docs/game-references/blips/
+                name = 'PC', -- Name of the Blip
+            },
+            marker = {
+                enable = true, -- will displayed
+                radius = 3.0, -- radius of marker
+                marker = 27, -- marker type see https://docs.fivem.net/docs/game-references/checkpoints/
+                scale = 1.0, -- scale of marker
+                rgba = { 255, 0, 0, 155 }, -- color of marker
+                subMarker = {
+                    marker = 21, -- marker type see https://docs.fivem.net/docs/game-references/markers/
+                    posZ = 1.0,  -- z position of the subMarker
+                },
+            },
+        },
+        keyToOpen = 57, -- Key when you want to open Authority Net from everywhere. See: https://docs.fivem.net/docs/game-references/controls/
     }
 }

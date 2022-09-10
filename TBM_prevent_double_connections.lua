8f6e35e8913d83783f802edc2da0ab707ffb5a02
Ralf Geisthardtz <RalfGe@gmail.com>
2022-09-10 09:31:33 +0200

    1.4.0

--- TBM_prevent_double_connections.lua
+++ TBM_prevent_double_connections.lua
@@ -13,6 +13,9 @@ Config = {
     banOptions = {
         banPlayerInTBMInGameWhiteList = false, -- If you have my mod: TBM In Game Whitelist (https://thebusters-shop.tebex.io/package/5070248). The player can be baned there.
     },
+    ignoreSteamId = false, -- if true steam id will not checked
+    ignoreDiscordId = false, -- if true discord id will not checked
+    ignoreLicenceId = false, -- if true licence id wil not checked
     EventsThatShouldTriggerDoubleConnectionDetected = { -- you can add here as many events you want. They will trigger a server side event with parameters: TriggerEvent(yourEventName, playerIdentityData), Take a look into readme for closer informations
         {
             name = 'yourEventName', -- The event name
